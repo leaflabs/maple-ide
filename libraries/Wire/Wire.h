4a030568295859b93644308d5e219d6820f6d501
Marti Bolivar <mbolivar@mit.edu>
2010-09-13 19:17:38 -0400

    some stuff implemented

--- libraries/Wire/Wire.h
+++ libraries/Wire/Wire.h
@@ -26,6 +26,8 @@
  *  @brief Wire library, ported from Arduino. Provides a lean interface to I2C (two-wire) communication
  */
 
+#include "wirish.h"
+
 #ifndef _WIRE_H_
 #define _WIRE_H_
 
@@ -34,9 +36,54 @@ typedef struct {
   uint8 sda;
 } Port;
 
+#define WIRE_BUFSIZ 32
+
+/* return codes from endTransmission() */
+#define SUCCESS = 0,        /* transmission was successful */
+#define EDATA = 1,          /* too much data */
+#define ENACKADDR = 2,      /* received nack on transmit of address */
+#define ENACKTRNS = 3,      /* received nack on transmit of data */
+#define EOTHER = 4          /* other error */
+
+#define SDA 6
+#define SCL 7
+
+#define I2C_WRITE 1
+#define I2C_READ 0
+
+static uint16 delay;
+
 class TwoWire {
  private:
+    static uint8* rx_buf;
+    static uint8 rx_buf_idx;
+    static uint8 rx_buf_len;
+
+    static uint8 tx_addr;
+    static uint8* tx_buf;
+    static uint8 tx_buf_idx;
+    static uint8 tx_buf_len;
+
+    Port port;
+
+    uint8 writeOneByte(uint8);
+    uint8 readOneByte(uint8, uint8*);  /* read/write bit is LSB, comes out first */
  public:
+    public TwoWire();
+    void begin();
+    void begin(uint8, uint8);
+    void beginTransmission(uint8);
+    void beginTransmission(int);
+    uint8 endTransmission(void);
+    uint8 requestFrom(uint8, uint8);
+    uint8 requestFrom(int, int);
+    void send(uint8);
+    void send(uint8*, uint8);
+    void send(int);
+    void send(int*, int);
+    void send(char*);
+    uint8 available();
+    uitn8 receive();
 }
 
 static void    i2c_start(Port port);
@@ -47,6 +94,6 @@ static void    i2c_send_nack(Port port);
 static uint8   i2c_shift_in(Port port);
 static void    i2c_shift_out(Port port, uint8 val);
 
+extern TwoWire Wire;
 
-extern TwoWire Wire; 
 #endif // _WIRE_H_
